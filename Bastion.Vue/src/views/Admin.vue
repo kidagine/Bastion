c1da709eb0ae99fbdad7cd915d09e30632685acf
kidagine <36992613+kidagine@users.noreply.github.com>
2019-10-15 10:14:41 +0200

    +Minor fix

--- Bastion.Vue/src/views/Admin.vue
+++ Bastion.Vue/src/views/Admin.vue
@@ -3,11 +3,18 @@
     <ul>
       <li v-for="context in contexts" v-bind:key="context.id">{{context}}</li>
     </ul>
-    <button v-on:click="loadContext()">Load</button>
-    <input v-model="del">
-    <button v-on:click="deleteContext()">Delete</button>
-    <button v-on:click="addContext()">Add</button>
-    <button v-on:click="editContext()">Edit</button>
+    <div class="crud-display">
+      <button v-on:click="loadContext()">Read</button>
+      <input spellcheck="false" v-model="contexts.id" placeholder="Id to delete">
+      <button v-on:click="deleteContext()">Delete</button>
+      <input spellcheck="false" v-model="contexts.name" placeholder="Name to add">
+      <input spellcheck="false" v-model="contexts.price" placeholder="Price to add">
+      <button v-on:click="addContext()">Add</button>
+      <input spellcheck="false" v-model="contexts.id" placeholder="Id to update">
+      <input spellcheck="false" v-model="contexts.name" placeholder="Name to update">
+      <input spellcheck="false" v-model="contexts.price" placeholder="Price to update">
+      <button v-on:click="editContext()">Edit</button>
+    </div>
   </div>
 </template>
 
@@ -15,135 +22,49 @@
 import axios from 'axios'
 export default {
   name: 'app',
-  //mounted() {
-  //  this.loadContent()
-  //},
   data: () => {
     return {
-      del: "",
-      contexts: [],
-      rubberDuck: {
-        "id": 16,
-        "duckColor": { "id": 1, "name": "Yellow" },
-        "duckPattern": { "id": 1, "name": "Spotted" },
-        "duckSize": 3,
-        "price": 678,
-        "duckGender": 1,
-        "duckCostume": { "id": 1, "name": "Navy" }
-      },
-      editRubberDuck: {
-        "id": 16,
-        "duckColor": { "id": 1, "name": "Yellow" },
-        "duckPattern": { "id": 1, "name": "Spotted" },
-        "duckSize": 3,
-        "price": 100,
-        "duckGender": 1,
-        "duckCostume": { "id": 1, "name": "Navy" }
-      }
+      contexts: []
     }
   },
   methods: {
     loadContext: function() {
-      axios.get('http://bastion-shop.azurewebsites.net/api/speakers/').then(response => (this.contexts = response.data))
+      axios.get('https://bastion-shop.azurewebsites.net/api/speakers').then(response => (this.contexts = response.data))
     },
     deleteContext: function() {
-      axios.delete('http://bastion-shop.azurewebsites.net/api/speakers/' + this.del)
+      if(confirm("Delete speaker of Id: " + this.contexts.id + "?")) 
+      {
+        axios.delete('http://world-of-oz.azurewebsites.net/api/colors/' + this.contexts.id)
+      }
     },
     addContext: function () {
-      axios.post('http://bastion-shop.azurewebsites.net/api/speakers/', this.rubberDuck)
+      axios.post('http://bastion-shop.azurewebsites.net/api/speakers', {
+        name: this.contexts.name,
+        price: this.contexts.price
+      }).then(() => {
+        alert("Speaker " + this.contexts.name + " added!");
+      });
     },
     editContext: function () {
-      axios.put('https://rubberduckshop.azurewebsites.net/api/duck/16', this.editRubberDuck)
+      axios.put('http://world-of-oz.azurewebsites.net/api/colors/' + this.contexts.id, {
+        id: this.contexts.id,
+        name: this.contexts.name,
+        update: this.contexts.price
+      }).then(() => {
+        alert("Speaker " + this.contexts.id + ": " + this.contexts.name + " updated!");
+      });
     }
   }
 }
 </script>
 
 <style>
-.crud-container{
-  padding-top: 5%;
-    width: 100%;
-    font-size: 12px;
-    overflow: hidden; /* contain floated elements */
-    background: #ccc
-}
-.post-container{
-  background-color: red;
-    float: left; 
-    width: 33.3%;
-    padding-right: 1%;
-    padding-left: 1%;}
-.delete-container{
-    background-color: red;
-    float: left; 
-    width: 33.3%;
-    padding-right: 1%;
-    padding-left: 1%;
-}
-.put-container{
-    background-color: red;
-    float: left; 
-    width: 33.3%;
-    padding-right: 1%;
-    padding-left: 1%;
-}
-
-
-.admin-product-list {
-  list-style: none;
-  margin: 20px auto;
-  width: 50%;
-}
-
-.admin-product-list-item {
-  border: 1px solid rgb(41, 41, 41);
-  border-radius: 5px;
-  text-align: center;
-  display: block;
-  box-shadow: 2px 2px rgba(138, 124, 124, 0.4);
-}
-#button{
-  overflow: hidden;
-  height: 3.57143rem;
-  line-height: 3.57143rem;
-  transition: all .15s ease;
-  font-family: "Helvetica Neue";
-  letter-spacing: .07143rem;
-  font-size: .85714rem;
-  text-transform: uppercase;
-  text-align: center;
-  background: 0 0;
-  color: #000;
-  background: #ededed;
-  z-index: 9;
-  width: 100%;
-    -o-transition:.5s;
-  -ms-transition:.5s;
-  -moz-transition:.5s;
-  -webkit-transition:.5s;
-  transition:.5s;
-}
-#button:hover{
-  background-color: black;
-  color:white;
-  cursor: pointer;    
-}
-#button:after {
-  content: "";
-  background: #e4007c;
-  display: block;
-  position: absolute;
-  padding-top: 300%;
-  padding-left: 350%;
-  margin-left: -20px!important;
-  margin-top: -120%;
-  opacity: 0;
-  transition: all 0.8s
-}
-#button:active:after {
-  padding: 0;
-  margin: 0;
-  opacity: 1;
-  transition: 0s
+ .crud-display{
+    display: block;
+    font-size:11px;
+    padding:4px 2px;
+    border:solid 1px #aacfe4;
+    width:150px;
+    margin:2px 0 20px 10px;
 }
 </style>
